012f321aaa71dce62c42b52648ea18665fde3e8b
m4sc0tech <m4sc0tech@gmail.com>
2026-08-18 00:50:13 -0500

    Update csv_writer.py

--- csv_writer.py
+++ csv_writer.py
@@ -1,1 +1,14 @@
-/home/matias/turtlebot3_ws/src/turtlebot3_logger/turtlebot3_logger/csv_writer.py
+import csv
+
+
+def open_csv_file(path):
+    """Open a CSV file for writing and return (file, writer)."""
+    csv_file = open(path, 'w', newline='')
+    writer = csv.writer(csv_file)
+
+    header = ['sec', 'nanosec'] + \
+        [f'range_{i}' for i in range(360)] + \
+        ['linear_x', 'angular_z']
+    writer.writerow(header)
+
+    return csv_file, writer
